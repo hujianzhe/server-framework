250a73bb2c4b1884e0c15f4f2b083c453e183c8e
hujianzhe <976784480@qq.com>
2020-06-12 01:00:32 +0800

    ...

--- ServiceCenter/req_cluster_list.c
+++ ServiceCenter/req_cluster_list.c
@@ -41,6 +41,7 @@ void reqClusterList(TaskThread_t* thrd, UserMsg_t* ctrl) {
 	cJSON_AddNewNumber(cjson_ret_root, "version", getClusterTableVersion());
 	cjson_ret_array_cluster = cJSON_AddNewArray(cjson_ret_root, "clusters");
 	for (lnode = getClusterList(ptr_g_ClusterTable())->head; lnode; lnode = lnode->next) {
+		cJSON* cjson_hashkey_arr;
 		Cluster_t* cluster = pod_container_of(lnode, Cluster_t, m_listnode);
 		cJSON* cjson_ret_object_cluster = cJSON_AddNewObject(cjson_ret_array_cluster, NULL);
 		if (!cjson_ret_object_cluster)
@@ -50,6 +51,13 @@ void reqClusterList(TaskThread_t* thrd, UserMsg_t* ctrl) {
 		cJSON_AddNewNumber(cjson_ret_object_cluster, "port", cluster->port);
 		cJSON_AddNewString(cjson_ret_object_cluster, "socktype", if_socktype2string(cluster->socktype));
 		cJSON_AddNewNumber(cjson_ret_object_cluster, "weight_num", cluster->weight_num);
+		cjson_hashkey_arr = cJSON_AddNewArray(cjson_ret_object_cluster, "hash_key");
+		if (cjson_hashkey_arr) {
+			unsigned int i;
+			for (i = 0; i < cluster->hashkey_cnt; ++i) {
+				cJSON_AddNewNumber(cjson_hashkey_arr, NULL, cluster->hashkey[i]);
+			}
+		}
 		if (!strcmp(cluster->ip, cjson_ip->valuestring) &&
 			cluster->port == cjson_port->valueint &&
 			cluster->socktype == if_string2socktype(cjson_socktype->valuestring))

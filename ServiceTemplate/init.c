fba4c31ce1d73df7d1445e2e7898ea5d4dfdbca1
hujianzhe <976784480@qq.com>
2020-06-18 00:48:39 +0800

    service load server config

--- ServiceTemplate/init.c
+++ ServiceTemplate/init.c
@@ -7,41 +7,6 @@
 #pragma comment(lib, "ServiceCommCode.lib")
 #endif
 
-static int service_center_check_connection_timeout_callback(RBTimer_t* timer, RBTimerEvent_t* e) {	
-	logInfo(ptr_g_Log(), __FUNCTION__);
-
-	do {
-		SendMsg_t msg;
-		Channel_t* sc_channel;
-		ClusterNodeGroup_t* sc_grp;
-		ClusterNode_t* sc_clsnd;
-		char* req_data;
-		int req_datalen;
-
-		if ('\0' == selfClusterNode()->name[0])
-			break;
-		sc_grp = getClusterNodeGroup(ptr_g_ClusterTable(), "ServiceCenter");
-		if (!sc_grp)
-			break;
-		sc_clsnd = pod_container_of(sc_grp->nodelist.head, ClusterNode_t, m_grp_listnode);
-		sc_channel = connectClusterNode(sc_clsnd);
-		if (!sc_channel)
-			break;
-		req_data = strFormat(&req_datalen, "{\"name\":\"%s\",\"ip\":\"%s\",\"port\":%u,\"weight_num\":%d,\"connection_num\":%d}",
-			selfClusterNode()->name, selfClusterNode()->ip, selfClusterNode()->port,
-			selfClusterNode()->weight_num, selfClusterNode()->connection_num);
-		if (!req_data)
-			break;
-		makeSendMsg(&msg, CMD_CLUSTER_HEARTBEAT, req_data, req_datalen);
-		channelSendv(sc_channel, msg.iov, sizeof(msg.iov) / sizeof(msg.iov[0]), NETPACKET_FRAGMENT);
-		free(req_data);
-	} while (0);
-
-	e->timestamp_msec = gmtimeMillisecond() + 1000 * 60;
-	rbtimerAddEvent(timer, e);
-	return 1;
-}
-
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -49,10 +14,50 @@ extern "C" {
 __declspec_dllexport int init(TaskThread_t* thrd, int argc, char** argv) {
 	ConfigConnectOption_t* option = NULL;
 	ClusterNode_t* clsnd;
-	RBTimerEvent_t* timeout_ev;
 	unsigned int i;
+	char* file_data;
+	const char* path;
+
+	//
+	path = ptr_g_Config()->extra_data_txt;
+	file_data = fileReadAllData(path, NULL);
+	if (!file_data) {
+		logErr(ptr_g_Log(), "fdOpen(%s) error", path);
+		return 0;
+	}
+	if (!loadClusterNodeFromJsonData(ptr_g_ClusterTable(), file_data)) {
+		free(file_data);
+		return 0;
+	}
+	free(file_data);
+
+	clsnd = getClusterNodeFromGroup(
+		getClusterNodeGroup(ptr_g_ClusterTable(), selfClusterNode()->name),
+		selfClusterNode()->socktype,
+		selfClusterNode()->ip,
+		selfClusterNode()->port
+	);
+	if (!clsnd) {
+		logErr(ptr_g_Log(), "%s cluster node self not find, name:%s, socktype:%s, ip:%s, port:%u",
+			__FUNCTION__,
+			selfClusterNode()->name,
+			if_socktype2string(selfClusterNode()->socktype),
+			selfClusterNode()->ip,
+			selfClusterNode()->port
+		);
+		return 0;
+	}
 
 	// listen port
+	if (selfClusterNode()->port) {
+		ReactorObject_t* o = openListenerInner(selfClusterNode()->socktype, selfClusterNode()->ip, selfClusterNode()->port);
+		if (!o) {
+			logErr(ptr_g_Log(), "listen failure, ip:%s, port:%u ......", option->ip, option->port);
+			return 0;
+		}
+		reactorCommitCmd(ptr_g_ReactorAccept(), &o->regcmd);
+	}
+
 	for (i = 0; i < ptr_g_Config()->listen_options_cnt; ++i) {
 		ConfigListenOption_t* option = ptr_g_Config()->listen_options + i;
 		ReactorObject_t* o;
@@ -68,43 +73,7 @@ __declspec_dllexport int init(TaskThread_t* thrd, int argc, char** argv) {
 		}
 		reactorCommitCmd(ptr_g_ReactorAccept(), &o->regcmd);
 	}
-	//
 
-	for (i = 0; i < ptr_g_Config()->connect_options_cnt; ++i) {
-		option = ptr_g_Config()->connect_options + i;
-		if (!strcmp(option->protocol, "ServiceCenter"))
-			break;
-	}
-	if (!option) {
-		logErr(ptr_g_Log(), "miss connect service config");
-		return 0;
-	}
-	clsnd = newClusterNode(option->socktype, option->ip, option->port);
-	if (!clsnd) {
-		logErr(ptr_g_Log(), "ServiceCenter newCluster error");
-		return 0;
-	}
-	if (!regClusterNode(ptr_g_ClusterTable(), option->protocol, clsnd)) {
-		logErr(ptr_g_Log(), "ServiceCenter regCluster error");
-		return 0;
-	}
-	if (!regNumberDispatch(CMD_DISTRIBUTE_CLUSTER_LIST, distributeClusterList)) {
-		logErr(ptr_g_Log(), "regNumberDispatch(CMD_DISTRIBUTE_CLUSTER_LIST, distributeClusterList) failure");
-		return 0;
-	}
-	if (!rpcReqClusterList(thrd, clsnd)) {
-		logErr(ptr_g_Log(), "rpcReqClusterList failure, ip:%s, port:%u ......", clsnd->ip, clsnd->port);
-		return 0;
-	}
-	timeout_ev = (RBTimerEvent_t*)malloc(sizeof(RBTimerEvent_t));
-	if (!timeout_ev) {
-		logErr(ptr_g_Log(), "malloc(sizeof(RBTimerEvent_t)) error");
-		return 0;
-	}
-	timeout_ev->arg = NULL;
-	timeout_ev->timestamp_msec = gmtimeMillisecond() + 1000 * 60;
-	timeout_ev->callback = service_center_check_connection_timeout_callback;
-	rbtimerAddEvent(&thrd->timer, timeout_ev);
 	return 1;
 }
 

92557d7fdab1206dfb5ea24cf84ec2b3c0fa463a
hujianzhe <976784480@qq.com>
2020-07-20 15:22:55 +0800

    optimized code

--- SoTest/test_handler.c
+++ SoTest/test_handler.c
@@ -167,8 +167,65 @@ void reqParallelTest2(TaskThread_t* thrd, UserMsg_t* ctrl) {
 void reqHttpUploadFile(TaskThread_t* thrd, UserMsg_t* ctrl) {
 	HttpFrame_t* httpframe = ctrl->httpframe;
 	ListNode_t* cur, *next;
+	char* reply;
+	int replylen;
+	const char* str_result;
+	const char basepath[] = "/data/";
 	for (cur = httpframe->multipart_form_datalist.head; cur; cur = next) {
+		const char* s, *e;
+		char* path;
+		FD_t fd;
+		int wrbytes;
 		HttpMultipartFormData_t* form_data = pod_container_of(cur, HttpMultipartFormData_t, listnode);
 		next = cur->next;
+
+		s = httpframeGetHeader(&form_data->headers, "Content-Disposition");
+		if (!s)
+			continue;
+		s = strstr(s, "filename=\"");
+		if (!s)
+			continue;
+		s += sizeof("filename=\"") - 1;
+		e = strchr(s, '"');
+		if (!e)
+			continue;
+		path = (char*)malloc(sizeof(basepath) - 1 + e - s);
+		if (!path)
+			break;
+		memcpy(path, basepath, sizeof(basepath) - 1);
+		memcpy(path + sizeof(basepath) - 1, s, e - s);
+		path[sizeof(basepath) - 1 + e - s] = 0;
+
+		fd = fdOpen(path, FILE_CREAT_BIT | FILE_WRITE_BIT | FILE_APPEND_BIT);
+		if (fd == INVALID_FD_HANDLE) {
+			logErr(ptr_g_Log(), "%s open failure", path);
+			free(path);
+			break;
+		}
+		logInfo(ptr_g_Log(), "%s open and ready write append %u bytes...", path, form_data->datalen);
+		
+		wrbytes = fdWrite(fd, form_data->data, form_data->datalen);
+		if (wrbytes != form_data->datalen) {
+			logErr(ptr_g_Log(), "%s write error", path);
+			free(path);
+			break;
+		}
+
+		fdClose(fd);
+		logInfo(ptr_g_Log(), "%s write %u bytes...", path, wrbytes);
+		free(path);
 	}
+	if (cur) {
+		str_result = "UPLOAD ERROR";
+	}
+	else {
+		str_result = "UPLOAD SUCCESS";
+	}
+	reply = strFormat(&replylen, HTTP_SIMPLE_RESP_FMT, HTTP_SIMPLE_RESP_VALUE(200, str_result, strlen(str_result)));
+	if (!reply) {
+		return;
+	}
+	channelSend(ctrl->channel, reply, replylen, NETPACKET_FRAGMENT);
+	reactorCommitCmd(NULL, &ctrl->channel->_.stream_sendfincmd);
+	free(reply);
 }

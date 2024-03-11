b604e1e09709a1e8d97dbb49d5a9cd270581998b
hujianzhe <976784480@qq.com>
2024-03-11 10:43:06 +0800

    use different signal exit

--- ServiceTemplate/main.c
+++ ServiceTemplate/main.c
@@ -3,9 +3,18 @@
 
 #if defined(_WIN32) || defined(_WIN64)
 #pragma comment(lib, "BootServer.lib")
+static int s_exit_signo = SIGINT;
+#else
+static int s_exit_signo = SIGTERM;
 #endif
 
 extern int init(BootServerGlobal_t* g);
+
+static void sig_proc(int signo) {
+	if (s_exit_signo == signo) {
+		stopBootServerGlobal();
+	}
+}
 
 int main(int argc, char** argv) {
 	if (argc < 2) {
@@ -17,15 +26,26 @@ int main(int argc, char** argv) {
 		fprintf(stderr, "initBootServerGlobal err:%s\n", getBSGErrmsg());
 		return 1;
 	}
+	// reg signal
+	if (!signalThreadMaskNotify()) {
+		fprintf(stderr, "main thread signalThreadMaskNotify err:%d\n", errnoGet());
+		goto err;
+	}
+	signalReg(s_exit_signo);
+	if (!runBootServerSignalHandler(sig_proc)) {
+		goto err;
+	}
 	// print boot cluster node info
 	printBootServerNodeInfo();
 	// run BootServer and wait BootServer end
-	runBootServerGlobal();
-	// print BootServer run error
-	if (getBSGErrmsg() && getBSGErrmsg()[0]) {
-		fputs(getBSGErrmsg(), stderr);
-		logErr(ptrBSG()->log, "%s", getBSGErrmsg());
+	if (!runBootServerGlobal()) {
+		goto err;
 	}
+	goto ret;
+err:
+	fputs(getBSGErrmsg(), stderr);
+	logErr(ptrBSG()->log, "%s", getBSGErrmsg());
+ret:
 	// free BootServer
 	freeBootServerGlobal();
 	return 0;
